d6db527060d49b73c8c85f7ff44ad4d31c7b3f80
Arsenica Sebastian <sarsenica537@gmail.com>
2023-03-04 19:28:02 +0200

    Add project files.

---
+++ AssetsAPI/Classes/User.cs
@@ -0,0 +1,19 @@
+﻿namespace AssetsAPI.Classes
+{
+    public class User
+    {
+        public int id { get; set; }
+
+        public string username { get; set; } = string.Empty;
+
+        public string fullName { get; set; } = string.Empty;
+
+        public string email { get; set; } = string.Empty; 
+
+        public byte[] passwordHash { get; set; } 
+
+        public byte[] passwordSalt { get; set; } 
+
+        public DateTime createDate { get; set; }
+    }
+}
